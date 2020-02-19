df23b37636d9728c3bd945101fede051d5297772
translate-guy <2891496009ds@gmail.com>
2020-02-19 23:46:20 +0000

    in team6.py we added our strategy function

--- team6.py
+++ team6.py
@@ -9,8 +9,8 @@
 
 team_name = 'zero heroes' # Only 10 chars displayed.
 
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'round four'
+strategy_description = 'betray 4 times after using collude facing betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -19,14 +19,13 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
-    # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
-    # The first round between these two players is my_history[0] and their_history[0].
-    # The most recent round is my_history[-1] and their_history[-1].
-    
-    # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
+  betray = 0
+  if my_history == 0:
     return 'c'
-
+  if their_history == 'b'
+    return 'b'
+    betray += !
+  elif betray %4 == 0:
+    return 'c'
+  else:
+    return 'c'
