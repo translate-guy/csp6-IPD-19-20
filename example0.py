055b2b5b136dd5aa9b0e608e102427aad009d113
translate-guy <2891496009ds@gmail.com>
2020-02-19 22:21:00 +0000

    in example0.py, we change the strategy into our own

--- example0.py
+++ example0.py
@@ -7,20 +7,16 @@
 ####
 
 team_name = 'E0'
-strategy_name = 'Collude'
-strategy_description = 'Always collude.'
+strategy_name = 'you dont wanna betray me'
+strategy_description = 'Collude until betrayed. If betrayed, betray for the rest.'
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    # This player always colludes.
+  '''collude until facing betray and betray for the rest'''
+
+  if my_history == 0:
+    return 'c'  
+  elif 'b' in their_history:
+    return 'b'
+  else:
     return 'c'
     
