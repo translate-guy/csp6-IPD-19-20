cebb709b62dc52e5cbc0f57e80a47058d8eebc0d
translate-guy <2891496009ds@gmail.com>
2020-02-20 21:34:03 +0000

    mistake of 1 into !

--- team6.py
+++ team6.py
@@ -24,7 +24,7 @@ def move(my_history, their_history, my_score, their_score):
     return 'c'
   if their_history == 'b'
     return 'b'
-    betray += !
+    betray += 1
   elif betray %4 == 0:
     return 'c'
   else:
